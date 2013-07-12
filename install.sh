e8c942de80f337640787d237e2a5dd47a93829f1
James <james@James-PC.(none)>
2013-07-12 17:43:04 +0100

    Updated install script to install vundle and jellybeans

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
+# Set up a symbolic link to the vimrc file
+echo "Linking vimrc..."
 ln -s ~/dotfiles/vimrc ~/.vim/vimrc
+
+# Install vundle
+echo "Installing vundle"
+cd ~/.vim/bundle
+git clone git://github.com/gmarik/vundle
+
+#Install jellybeans
+echo "Installing jellybeans"
+cd ~/.vim/colors
+wget https://raw.github.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim 
+
+
